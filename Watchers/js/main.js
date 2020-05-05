907a878510ecf318a9b0ed6614485638cdab726c
AkitoTsukahara <tsukahara.akito@gmail.com>
2020-05-05 22:28:00 +0900

    [add]Watchers

--- Watchers/js/main.js
+++ Watchers/js/main.js
@@ -1,16 +1,25 @@
 new Vue({
     el: '#app',
     data: {
-        basePrice: 100
+        km: 0,
+        m: 0,
+        mm: 0
     },
-    computed:{
-        taxIncludedPrice: {
-            get: function() {
-                return parseInt(this.basePrice * 1.1)
-            },
-            set: function(taxIncludedPrice){
-                this.basePrice = Math.ceil(taxIncludedPrice / 1.1)
-            }
+    watch: {
+        km: function(value){
+            this.km = value
+            this.m = value*1000
+            this.mm = value*1000000
+        },
+        m: function(value){
+            this.km = value/1000
+            this.m = value
+            this.mm = value*1000
+        },
+        mm: function(value){
+            this.km = value/1000000
+            this.m = value/1000
+            this.mm = value
         }
     }
 })
