c8e1d12446f7b39779a01b2f7255db881bbf46dd
AkitoTsukahara <tsukahara.akito@gmail.com>
2020-05-05 22:26:53 +0900

    [add]BitCoinApp

--- BitCoinApi/js/main.js
+++ BitCoinApi/js/main.js
@@ -1,28 +1,25 @@
 new Vue({
     el: '#app',
     data: {
-        newItem: '',
-        todos: [],
+         bpi: null,
+        hasError: false,
+        loading: true
     },
-    methods: {
-        addItem: function(event){
-            // alert();
-
-            //未入力判定
-            if(this.newItem == '') return
-
-            let todo = {
-                item: this.newItem,
-                isDone: false
-            };
-
-            this.todos.push(todo)
-
-            //入力内容をリセット
-            this.newItem = ''
-        },
-        deleteItem: function(index){
-            this.todos.splice(index,1)
+    mounted: function(){
+        axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+            .then(function(response){
+                this.bpi = response.data.bpi
+            }.bind(this))
+            .catch(function (error) {
+                this.hasError = true
+            }.bind(this))
+            .finally(function (){
+                this.loading = false
+            }.bind(this))
+    },
+    filters: {
+        currencyDecimal(value){
+            return value.toFixed(2)
         }
     }
 })
